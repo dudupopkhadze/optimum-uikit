f0f4143e939cfb5b8a2b7f7e9e848888709c68d4
Dudu Popkhadze <dudupopkhadze@gmail.com>
2023-03-05 22:04:04 +0400

    implement button

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
 
 export interface ButtonProps {
   text: string;
@@ -6,5 +7,23 @@ export interface ButtonProps {
 }
 
 export const Button = (props: ButtonProps) => {
-  return <></>;
+  return (
+    <TouchableOpacity onPress={props.onPress}>
+      <View style={sheet.container}>
+        <Text style={sheet.text}>{props.text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
+
+const sheet = StyleSheet.create({
+  container: {
+    padding: 12,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+    maxWidth: 200,
+    borderRadius: 20,
+  },
+  text: { color: "white", fontSize: 18 },
+});
